e1d59018150f30af35571e81058f56c6459fb1e8
aunz <aunz10@yahoo.com>
2018-07-22 20:10:41 +1200

    network analysis

--- mapNetwork.R
+++ mapNetwork.R
@@ -7,13 +7,13 @@ library(igraph)
 setwd(getSrcDirectory(function () {}))
 df.all = readRDS('./tmp/df.all.rds')
 
-
-
+# directed net
 net = graph_from_data_frame(
   d = df.all[from != '' & to != '', .N, .(from, to)][, .(source = from, target = to, weight = N)],
   directed = T
 )
 
+# a smaller set for testing out codes
 # net2 = graph_from_data_frame(
 #   d = df.all[from != '' & to != '', .N, .(from, to)]
 #     [, .(source = from, target = to, weight = N)]
@@ -25,17 +25,31 @@ net = graph_from_data_frame(
 
 # plot it
 plot(
-  net2,
+  net,
   edge.arrow.size = 0,
   edge.color = '#00000055',
-  edge.width = log(E(net2)$weight + 1),
+  edge.width = log(E(net)$weight + 1),
   vertex.frame.color = '#AAAAAA',
   vertex.label.color = '#333333',
   vertex.label.cex = 0.5,
   vertex.label.dist = 2,
   vertex.label = NA,
-  # layout = layout.circle(net2),
-  # layout = layout.sphere(net2),
-  # layout = layout.fruchterman.reingold(net2),
-  layout = layout.kamada.kawai(net2),
+  # layout = layout.circle(net),
+  # layout = layout.sphere(net),
+  # layout = layout.fruchterman.reingold(net),
+  layout = layout.kamada.kawai(net),
 )
+
+# Some descriptive stats
+# http://kateto.net/networks-r-igraph
+
+ecount(net) / (vcount(net) * (vcount(net) - 1))
+reciprocity(net)
+dyad_census(net)
+transitivity(net, type = 'global')
+transitivity(net, type = 'local')
+triad_census(net)
+
+diameter(net, directed = T)
+
+degree(net, mode = 'all')
